c04c0b9bfd93e4b701d03faaae58385581abc599
Ambarish Satheesh <ambarishsatheesh@hotmail.co.uk>
2020-05-15 03:26:37 +0100

    -add CLI interactivity to create new torrent and save to file -add skeleton of starting torrent download - only user file input

--- main.cpp
+++ main.cpp
@@ -28,6 +28,8 @@
 #include <sstream>
 #include <iostream>
 #include <stdexcept>
+#include <algorithm>
+#include <set>
 
 #include "trackerUrl.h"
 
@@ -35,17 +37,330 @@ using namespace Bittorrent;
 using namespace torrentManipulation;
 using namespace Decoder;
 
-int main(int argc, char* argv[])
+int main()
 {
-	if (argc < 2) {
-		std::cerr << "Too few arguments: needs a file name..\n";
-		return -1;
+
+	std::cout << "Welcome to my test Bittorrent client!" << "\n" << "\n";
+
+	std::cout << "1. Choose a torrent file and start downloading" << "\n";
+	std::cout << "2. Create a new torrent and start seeding" << "\n" << "\n";
+
+	std::string functionInputStr = "";
+	bool isValidInput = false;
+
+	while (!isValidInput)
+	{
+		std::cout << "\n" << "Please enter the number associated with the action you wish to " <<
+			"perform:" << "\n" << "\n";
+
+		std::cin >> functionInputStr;
+
+		if (functionInputStr == "1" || functionInputStr == "2")
+		{
+			std::cout << "\n" << "\n";
+			isValidInput = true;
+		}
+	}
+	isValidInput = false;
+
+	//need to declare this outside to it can be used out of local scope
+	std::string sourceFilePath = "";
+	std::string targetPath = "";
+	const char* pathPtr = "";
+	std::set<std::string> trackerSet;
+	bool isPrivate = 0;
+	std::string comment = "";
+	std::string name = "";
+
+	if (functionInputStr == "1")
+	{
+		while (!isValidInput)
+		{
+			std::cout << "\n" << "Please enter the torrent file path " <<
+				"(including file name and extension) to begin downloading:"
+				<< "\n" << "\n";
+
+			std::cin >> sourceFilePath;
+
+			pathPtr = sourceFilePath.c_str();
+
+			if (boost::filesystem::exists(pathPtr))
+			{
+				if (boost::filesystem::is_regular_file(pathPtr))
+				{
+					boost::filesystem::path p(sourceFilePath);
+					if (p.extension().string() == ".torrent")
+					{
+						std::cout << "Torrent file found." << "\n";
+
+						isValidInput = true;
+					}
+					else
+					{
+						std::cout << "\n" << "You did not provide a torrent file! " << "\n";
+					}
+				}
+				else
+				{
+					std::cout << "\n" << "You provided a directory path! " << "\n";
+				}
+			}
+			else
+			{
+				std::cout << "\n" << "This path does not exist! " << "\n";
+			}
+		}
+
+		isValidInput = false;
+			
+	}
+	else if (functionInputStr == "2")
+	{
+		//source path input
+		while (!isValidInput)
+		{
+			std::cout << "Please enter the path of the file or folder " <<
+				"you wish to create a torrent of:" << "\n" << "\n";
+
+			std::cin >> sourceFilePath;
+
+			pathPtr = sourceFilePath.c_str();
+
+			if (boost::filesystem::exists(pathPtr))
+			{
+				if (boost::filesystem::is_directory(pathPtr))
+				{
+					//get parent folder and store as name
+					if (sourceFilePath.back() == '\\')
+					{
+						boost::filesystem::path p(sourceFilePath);
+						name =  p.parent_path().filename().string();
+					}
+					else
+					{
+						sourceFilePath += '\\';
+						boost::filesystem::path p(sourceFilePath);
+						name = p.parent_path().filename().string();
+					}
+
+					std::cout << "Folder successfully processed." << "\n";
+					isValidInput = true;
+				}
+				else
+				{
+					std::cout << "File successfully processed." << "\n" << "\n";
+					isValidInput = true;
+				}
+			}
+			else
+			{
+				std::cout << "\n" << "This path does not exist! " <<
+					"Please enter in a valid file/folder path." << "\n" << "\n";
+			}
+		}
+
+		isValidInput = false;
+
+		//target path input
+		while (!isValidInput)
+		{
+			std::cout << "Please enter a target file path to save to:" 
+				<< "\n" << "\n";
+
+			std::cin >> targetPath;
+
+			const char* targetPtr = targetPath.c_str();
+
+			auto targetDirectory = getFileDirectory(targetPtr);
+
+			boost::filesystem::path p(targetPath);
+
+			if (boost::filesystem::is_directory(targetDirectory) &&
+				boost::filesystem::exists(targetDirectory))
+			{
+				if (targetDirectory == getFileDirectory(pathPtr))
+				{
+					std::cout << "\n" << "Target torrent file cannot be " <<
+						"in the same location as the source file." << "\n";
+				}
+				if (boost::filesystem::is_directory(targetPath))
+				{
+					std::cout << "Target needs to be a file!" << "\n";
+				}
+				if (p.extension().string() == ".torrent")
+				{
+					std::cout << "Target path successfully set to " 
+						<< targetPath << "\n" << "\n";
+
+					isValidInput = true;
+				}
+			}
+			else
+			{
+				std::cout << "\n" << "Invalid file path! "
+					<< "(Tip: Make sure the extension is \".torrent\")" 
+					<< "\n";
+			}
+		}
+
+		isValidInput = false;
+
+		//tracker input
+		while (!isValidInput)
+		{
+			std::string trackerInput = "";
+
+			std::cout << "Please enter a tracker one by one and enter \"done\" " <<
+				"when complete. Type \"remove\" to remove a tracker." << "\n" << "\n";
+
+			std::cin >> trackerInput;
+
+			std::string trackerCriteria1 = "udp://";
+			std::string trackerCriteria2 = "http://";
+
+			if (trackerInput == "done" && !trackerSet.empty())
+			{
+				std::cout << "\n" << "Trackers added: \n";
+				for (auto i : trackerSet)
+				{
+					std::cout << i << "\n";
+				}
+				std::cout << "\n" << "\n";
+
+				isValidInput = true;
+			}
+			else if (trackerInput == "done" && trackerSet.empty())
+			{
+				std::cout << "\n" << "No trackers have been added. " <<
+					"At least one tracker is required." << "\n";
+			}
+			else if (trackerInput.find(trackerCriteria1) == 0 ||
+				trackerInput.find(trackerCriteria2) == 0)
+			{
+				if (trackerSet.insert(trackerInput).second == false)
+				{
+					std::cout << "\n" << "Tracker already added to torrent!" 
+						<< "\n";
+				}
+				else
+				{
+					std::cout << "\n" << "Tracker added to torrent!" << "\n";
+					std::cout << "\n" << "Current tracker list: \n";
+
+					for (auto i : trackerSet)
+					{
+						std::cout << i << "\n";
+					}
+					std::cout << "\n";
+				}
+			}
+			else if (trackerInput.find("https") == 0)
+			{
+				std::cout << "\n" << "This Bittorrent client does not support HTTPS "
+					<< "trackers." << "\n";
+			}
+			else if (trackerInput == "remove")
+			{
+				if (trackerSet.empty())
+				{
+					std::cout << "\n" << "No trackers to remove. " << "\n";
+					continue;
+				}
+
+				std::cout << "\n" << "Enter the tracker to remove:" << "\n";
+
+				std::cin >> trackerInput;
+				if (trackerSet.count(trackerInput))
+				{
+					trackerSet.erase(trackerInput);
+
+					std::cout << "\n" << "Tracker removed from torrent!" << "\n";
+					std::cout << "\n" << "Current tracker list: \n";
+
+				}
+				else
+				{
+					std::cout << "\n" << "Remove failed. That tracker "
+						<< "had not been previously added." << "\n";
+				}
+
+				std::cout << "\n" << "Current tracker list: \n";
+				for (auto i : trackerSet)
+				{
+					std::cout << i << "\n";
+				}
+				std::cout << "\n";
+			}
+			else
+			{
+				std::cout << "\n" << "Invalid tracker protocol! (Tip: Tracker needs to begin "
+					<< "with \"udp://\" or \"http://\")" << "\n";
+			}
+		}
+
+		isValidInput = false;
+
+		//private torrent input
+		while (!isValidInput)
+		{
+			std::string isPrivateStr = "";
+
+			std::cout << "Please enter \"true\" if the torrent should " <<
+				"only use these trackers, and \"false\" if DHT/PEX can be used." 
+				<< "\n" << "\n";
+			
+			std::cin >> isPrivateStr;
+
+			if (isPrivateStr == "true" || isPrivateStr == "false"
+				|| isPrivateStr == "0" || isPrivateStr == "1")
+			{
+				if (isPrivateStr == "true" || isPrivateStr == "1")
+				{
+					isPrivate = true;
+					isValidInput = true;
+					std::cout << "Torrent status set to private." << "\n" << "\n";
+				}
+				else
+				{
+					isPrivate = false;
+					isValidInput = true;
+					std::cout << "Torrent status set to public." << "\n" << "\n";
+				}
+			}
+			else
+			{
+				std::cout << "\n" << "Invalid input." << "\n";
+			}
+		}
+
+		isValidInput = false;
+
+		//comment input
+		while (!isValidInput)
+		{
+			std::cout << "Please enter a torrent comment:" << "\n" << "\n";
+
+			std::getline(std::cin, comment);
+
+			if (!comment.empty())
+			{
+				isValidInput = true;
+			}
+			
+			std::cout << "Comment added." << "\n" << "\n";
+		}
 	}
 
-	const char* fullFilePath = argv[1];
 
-	//std::string test = "udp://tracker.coppersurfer.tk:6969";
-	//trackerUrl::trackerUrl(test);
+	//copy tracket set to vector
+	std::vector<std::string> trackerVec;
+	std::copy(trackerSet.begin(), trackerSet.end(), std::back_inserter(trackerVec));
+	const char* fullPathptr = sourceFilePath.c_str();
+	Torrent temp = createNewTorrent(name, fullPathptr, targetPath, isPrivate, comment, trackerVec);
+
+
+
+	/*const char* fullFilePath = argv[1];
 
 	bool isPrivate = false;
 
@@ -55,33 +370,10 @@ int main(int argc, char* argv[])
 	trackers.push_back("https://www.youtube.com/watch?v=q86g1aop6a8");
 	trackers.push_back("http://www.reddit.com/");
 
-	Torrent temp = createNewTorrent("test", fullFilePath, isPrivate, "test comment", trackers);
+	Torrent temp = createNewTorrent("test", fullFilePath, isPrivate, "test comment", trackers);*/
 
-	//std::cout << "file name: " << temp.generalData.fileName << std::endl;
-	//std::cout << "comment: " << temp.generalData.comment << std::endl;
-	//std::cout << "createdBy: " << temp.generalData.createdBy << std::endl;
-	//std::cout << "creationDate: " << temp.generalData.creationDate << std::endl;
-	//std::cout << "encoding: " << temp.generalData.encoding << std::endl;
-	//std::cout << "isPrivate: " << temp.generalData.isPrivate << std::endl << std::endl;
 
-	//std::cout << "trackers: " << std::endl;
-	//for (auto i : temp.generalData.trackerList)
-	//{
-	//	std::cout << i.trackerAddress << std::endl;
-	//}
-
-	//std::cout << std::endl;
-
-	//std::cout << "File info:" << std::endl << std::endl;
-
-	//for (size_t i = 0; i < temp.fileList.size(); ++i)
-	//{
-	//	std::cout << "file name: " << temp.fileList.at(i).filePath << "; ";
-	//	std::cout << "original file size: " << temp.fileList.at(i).fileSize << "; ";
-	//	std::cout << "readable file size: " << temp.fileList.at(i).readableFileSize << std::endl;
-	//}
-
-	std::string buffer = loadFromFile(fullFilePath);
+	/*std::string buffer = loadFromFile(fullFilePath);
 
 	std::random_device dev;
 	std::mt19937 rng(dev());
@@ -103,7 +395,7 @@ int main(int argc, char* argv[])
 	std::cout << "Tracker: " << udpTestTorrent.generalData.trackerList[4].trackerAddress << std::endl;
 
 	udpTestTorrent.generalData.trackerList[4].update(trackerObj::trackerEvent::started,
-		testClientID, 6681, udpTestTorrent.hashesData.urlEncodedInfoHash, udpTestTorrent.hashesData.infoHash, 2500, 1200, 0);
+		testClientID, 6681, udpTestTorrent.hashesData.urlEncodedInfoHash, udpTestTorrent.hashesData.infoHash, 2500, 1200, 0);*/
 
 	/*Torrent testTorrent = toTorrentObj(fullFilePath, torrent);
 	testTorrent.generalData.trackerList[0].update(trackerObj::trackerEvent::started, 
