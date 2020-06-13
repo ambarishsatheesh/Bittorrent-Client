c2258d9ad0ae9aa79b8740b6b01f6543d3e7d754
Ambarish Satheesh <ambarishsatheesh@hotmail.co.uk>
2020-06-13 18:39:27 +0100

    -replace getter variables with public functions that generate data when required, and define them
    -change torrentpieces parameter to shared ptr and change relevant constructors as required

--- src/TorrentStatus.cpp
+++ src/TorrentStatus.cpp
@@ -2,18 +2,52 @@
 
 namespace Bittorrent
 {
-	TorrentStatus::TorrentStatus(const TorrentPieces& pieces, const valueDictionary& torrent)
-		: remaining{ setRemaining(pieces) }
+    TorrentStatus::TorrentStatus(std::shared_ptr<TorrentPieces> pieces, const valueDictionary& torrent)
+        : ptr_piecesData(pieces)
 	{
 	}
 
-	TorrentStatus::TorrentStatus(const TorrentPieces& pieces)
+    TorrentStatus::TorrentStatus(std::shared_ptr<TorrentPieces> pieces)
+        : ptr_piecesData(pieces)
 	{
 
 	}
 
-	long TorrentStatus::setRemaining(const TorrentPieces& pieces)
+    int TorrentStatus::verifiedPiecesCount()
+    {
+        return std::count(isPieceVerified.cbegin(),
+                    isPieceVerified.cend(), true);
+    }
+
+    float TorrentStatus::verifiedRatio()
+    {
+        return verifiedPiecesCount() / float(ptr_piecesData->pieceCount);
+    }
+
+    bool TorrentStatus::isCompleted()
+    {
+        return verifiedPiecesCount() == ptr_piecesData->pieceCount;
+    }
+
+    bool TorrentStatus::isStarted()
+    {
+        return verifiedPiecesCount() > 0;
+    }
+
+    long long TorrentStatus::downloaded()
+    {
+        return ptr_piecesData->pieceSize * verifiedPiecesCount();
+    }
+
+    long long TorrentStatus::uploaded()
+    {
+        //need to implement properly
+        return 0;
+    }
+
+
+    long long TorrentStatus::remaining()
 	{
-		return pieces.totalSize - downloaded;
+        return ptr_piecesData->totalSize - downloaded();
 	}
 }
