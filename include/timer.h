0aa7d9db05474bf5d8886ccfa38b37a4605c15ec
Ambarish Satheesh <ambarishsatheesh@hotmail.co.uk>
2020-06-18 13:09:44 +0100

    -refactor timer class to use multiset and pairs of pointers to tracker and torrent objects

--- include/timer.h
+++ include/timer.h
@@ -9,23 +9,44 @@
 #include <functional>
 #include <mutex>
 #include <condition_variable>
+#include <set>
 
 namespace Bittorrent {
 
 using ptr_vec = std::shared_ptr<std::vector<std::shared_ptr<Torrent>>>;
 
+using pair = std::pair<trackerObj*, Torrent*>;
+
+//set comparator to order by ascending peer request interval
+struct compare
+{
+    bool operator()(const pair& t1, const pair& t2) const
+    {
+        if (t1.first->peerRequestInterval == t2.first->peerRequestInterval)
+        {
+            return t1.second->generalData.fileName < t2.second->generalData.fileName;
+        }
+        return t1.first->peerRequestInterval < t2.first->peerRequestInterval;
+    }
+};
+
+using pSet = std::multiset<pair, compare>;
+
+
 class TrackerTimer
 {
 public:
-    TrackerTimer(size_t time, ptr_vec torrentVec);
+    TrackerTimer(pSet* set, std::vector<byte> clientID);
     ~TrackerTimer();
 
 private:
-    void wait_then_call();
     std::mutex mtx;
     std::condition_variable cv{};
     std::thread wait_thread;
-    ptr_vec ptr_torrentVec;
+
+    std::vector<byte> clientID;
+    std::chrono::seconds tempTime;
+    void wait_then_call(pSet* trackerSet);
 };
 
 }
--- src/timer.cpp
+++ src/timer.cpp
@@ -3,10 +3,10 @@
 namespace Bittorrent {
 
 
-TrackerTimer::TrackerTimer(size_t time, ptr_vec torrentVec)
-    : wait_thread{ }, ptr_torrentVec{std::move(torrentVec)}
+TrackerTimer::TrackerTimer(pSet* trackerSet, std::vector<byte> clientID)
+    : clientID{clientID}, tempTime{0}
 {
-     wait_thread = std::thread{&TrackerTimer::wait_then_call, this};
+     wait_thread = std::thread(&TrackerTimer::wait_then_call, this, trackerSet);
 }
 
 TrackerTimer::~TrackerTimer()
@@ -17,13 +17,34 @@ TrackerTimer::~TrackerTimer()
     }
 }
 
-void TrackerTimer::wait_then_call()
+void TrackerTimer::wait_then_call(pSet* trackerSet)
 {
-    std::chrono::seconds time;
-    std::unique_lock<std::mutex> lck{ mtx };
-    for (auto tracker : *ptr_torrentVec)
+    while (!trackerSet->empty())
     {
-        cv.wait_for(lck, time);
+        loguru::set_thread_name("Timer Thread");
+        LOG_F(INFO, "Beginning tracker update...");
+
+        std::unique_lock<std::mutex> lck{ mtx };
+        cv.wait_for(lck, trackerSet->begin()->
+                    first->peerRequestInterval - tempTime);
+
+        //update first tracker in set
+        trackerSet->begin()->first->
+                update(TorrentStatus::currentStatus::started,
+                       clientID, 0,
+                       trackerSet->begin()->second->hashesData.urlEncodedInfoHash,
+                       trackerSet->begin()->second->hashesData.infoHash,
+                       trackerSet->begin()->second->statusData.uploaded(),
+                       trackerSet->begin()->second->statusData.downloaded(),
+                       trackerSet->begin()->second->statusData.remaining());
+
+        //re-insert element (with updated peer interval)
+        trackerSet->emplace(std::make_pair(trackerSet->begin()->first, trackerSet->begin()->second));
+
+        tempTime = trackerSet->begin()->first->peerRequestInterval;
+
+        //remove first element so next tracker can update
+        trackerSet->erase(trackerSet->begin());
     }
 }
 
