3f8663e2e196a4d6b1e8701de2f351b505d67cd6
Ambarish Satheesh <ambarishsatheesh@hotmail.co.uk>
2020-07-07 19:48:54 +0100

    -add variables to track download speed per torrent

--- src/TorrentStatus.cpp
+++ src/TorrentStatus.cpp
@@ -5,7 +5,9 @@ namespace Bittorrent
     TorrentStatus::TorrentStatus(TorrentPieces& pieces)
         : currentState{currentStatus::stopped}, ptr_piecesData(&pieces),
           pieceCount{0}, pieceSize{0}, blockSize{0}, totalSize{0},
-          acquiredBlocksCount{0}, downloadSpeed{0}, uploaded{0}
+          acquiredBlocksCount{0}, uploaded{0}, dataIntervalTotal{0},
+          lastReceivedTime{std::chrono::high_resolution_clock::time_point::min()},
+          downloadSpeed{0}
 	{
 	}
 
